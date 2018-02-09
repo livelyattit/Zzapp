764bb1865ce021e954ffa823aad22d7eb562e1ff
livelyattit <affan_94@hotmail.com>
2018-02-09 21:58:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/zzapp/ABC.java
@@ -0,0 +1,8 @@
+package com.example.zzapp;
+
+/**
+ * Created by affan_ui693t9 on 2/3/2018.
+ */
+
+public class ABC extends MainActivity {
+}
---
+++ app/src/main/java/com/example/zzapp/aa.java
@@ -0,0 +1,16 @@
+package com.example.zzapp;
+
+/**
+ * Created by affan_ui693t9 on 2/4/2018.
+ */
+
+class aa {
+    private static final aa ourInstance = new aa();
+
+    static aa getInstance() {
+        return ourInstance;
+    }
+
+    private aa() {
+    }
+}
